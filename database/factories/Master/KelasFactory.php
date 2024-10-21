102e94948048b53f9cfcb21c312a9f4eb9449e53
Daffa Alif <daffa4lif@gmail.com>
2024-10-21 22:29:14 +0700

    feat: kelas schema and seeder

---
+++ database/factories/Master/KelasFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories\Master;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Master\Kelas>
+ */
+class KelasFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'jurusan' => fake()->boolean() ? 'IPS' : 'IPA',
+            'nama' => 'kelas - ' . rand(1, 100)
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (\Illuminate\Support\Facades\App::isLocal()) {
+            // kelas seeder
+            \App\Models\Master\Kelas::factory(5)->create();
+        }
     }
 }
