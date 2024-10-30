08bb6cfd17ac815e94e903fe201718646a90ecef
Daffa Alif <daffa4lif@gmail.com>
2024-10-30 13:30:53 +0700

    fix: minnor issue

--- resources/views/pages/absen/index.blade.php
+++ resources/views/pages/absen/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
 <header class="mb-5">
-    <x-main-header title="List Siswa" />
+    <x-main-header title="List Absen" />
     <div class="flex justify-end">
         <button data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button">
             Tambah
