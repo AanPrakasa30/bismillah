60159aed0017ac6fb46aeecdbc71bc84f5e59ad5
Daffa Alif <daffa4lif@gmail.com>
2024-11-08 15:52:57 +0700

    feat: registere siswa to kelas

--- app/Services/SiswaServiceImpl.php
+++ app/Services/SiswaServiceImpl.php
@@ -82,4 +82,82 @@ class SiswaServiceImpl implements SiswaService
 
         return $reports;
     }
+
+    public function registerKelasSiswaBySpreadsheetTemp(int $folderTemp, \App\Models\Master\Kelas $kelas, int $tahun): array
+    {
+        // define range file cols
+        $columnRange = [
+            'first' => 'A',
+            'last' => 'D'
+        ];
+
+        $pathTemp = $this->fileService->getTempPathFile($folderTemp);
+
+        $worksheet = $this->spService->read($pathTemp)->getActiveSheet();
+
+        // validate file, data header
+        $this->spService->validateColumnNames($worksheet, $columnRange['first'], $columnRange['last'], [
+            'NO', // 1
+            'NIS', // 2
+            'NAMA', // 3
+            'KELAMIN' // 4
+        ]);
+
+        // export data worksheet
+        $worksheetDatas = $this->spService->exportsDataCellInto2DArray($worksheet, $columnRange['first'], $columnRange['last']);
+        array_shift($worksheetDatas); // delete header cols
+
+        // cleaning null baris
+        $datas = array_filter($worksheetDatas, function ($subArray) {
+            return array_filter($subArray) !== []; // Cek apakah ada nilai non-null di sub-array
+        });
+
+        try {
+            DB::beginTransaction();
+
+            $reports = [];
+            foreach ($datas as $key => $siswaDatas) {
+                $siswa = Siswa::with("kelas")->where('NIS', (int) $siswaDatas['2'])->first();
+
+                if ($siswa) {
+                    if ($siswa->kelas->count() >= 1) {
+                        foreach ($siswa->kelas as $siswaKelas) {
+                            if ($siswaKelas->pivot->tahun == $tahun or ($siswaKelas->jurusan == $kelas->jurusan && $siswaKelas->nama == $kelas->nama && $siswaKelas->pivot->tahun == $tahun)) {
+                                $reports['error']['duplicate'][] = $siswaDatas;
+                            } else {
+                                \App\Models\Relasi\KelasSiswa::create([
+                                    "siswa_id" => $siswa->id,
+                                    "kelas_id" => $kelas->id,
+                                    "tahun" => $tahun
+                                ]);
+
+                                $reports['create'][] = $siswaDatas;
+                            }
+                        }
+                    } else {
+                        \App\Models\Relasi\KelasSiswa::create([
+                            "siswa_id" => $siswa->id,
+                            "kelas_id" => $kelas->id,
+                            "tahun" => $tahun
+                        ]);
+
+                        $reports['create'][] = $siswaDatas;
+                    }
+                } else {
+                    $reports['error']['unknow'][] = $siswaDatas;
+                }
+            }
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            throw $th;
+        }
+
+        // delete temp file and data
+        $this->fileService->deleteTempData($folderTemp);
+
+        return $reports;
+    }
 }
